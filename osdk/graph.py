ce48c48abd9429d32a3708a25bfd1c47e97cfda9
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2023-02-06 13:25:12 +0100

    Improved graph layout.

--- osdk/graph.py
+++ osdk/graph.py
@@ -3,20 +3,23 @@ from graphviz import Digraph
 from osdk.context import Context
 
 
-def view(context: Context):
+def view(context: Context, showExe: bool = False):
     g = Digraph(context.target.id, filename='graph.gv')
-    
+
+    g.attr('graph',  splines='ortho', concentrate='true')
     g.attr('node', shape='ellipse')
 
     for instance in context.instances:
-        if not instance.isLib():
+        if not instance.isLib() and not showExe:
             continue
+
         g.node(instance.manifest.id, f"<<B>{instance.manifest.id}</B><BR/>{instance.manifest.decription}>",
-               shape="plaintext", style="filled")
+               shape="plaintext", style="filled", fillcolor="lightgrey" if instance.isLib() else "lightblue")
+
         for req in instance.manifest.requires:
-            g.edge(req, instance.manifest.id)
+            g.edge(instance.manifest.id, req)
 
         for req in instance.manifest.provides:
-            g.edge(instance.manifest.id, req)
+            g.edge(req, instance.manifest.id)
 
     g.view(filename=f"{context.builddir()}/graph.gv")
