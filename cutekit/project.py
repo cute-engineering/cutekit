a3b5c33494592ff0b6187781eaeb0a7a05dcb2c1
Raphael <81994075+RaphMar2021@users.noreply.github.com>
2023-10-31 00:30:30 +0100

    project: Fix infinite loop on root() when running on Windows

--- cutekit/project.py
+++ cutekit/project.py
@@ -1,15 +1,13 @@
 import os
 from typing import Optional
 
-
 def root() -> Optional[str]:
     cwd = os.getcwd()
-    while cwd != "/":
+    while cwd != "/" and cwd != os.path.splitdrive(cwd)[0]:
         if os.path.isfile(os.path.join(cwd, "project.json")):
             return cwd
         cwd = os.path.dirname(cwd)
     return None
-
 
 def chdir() -> None:
     projectRoot = root()
