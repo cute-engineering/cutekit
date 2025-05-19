bbed55846c0ef75b38042e37b541a321d046ad13
sleepy-monax <me@smnx.sh>
2025-05-19 13:22:28 +0200

    Only install one package and not all the stuff.

--- cutekit/package.py
+++ cutekit/package.py
@@ -4,6 +4,7 @@ import os
 
 
 class PackageArgs(model.TargetArgs):
+    component: str = cli.operand("component", "Component to package")
     prefix: str = cli.arg(None, "prefix", "Installation prefix")
     layout: str = cli.arg(None, "layout", "Installation layout")
 
@@ -20,8 +21,20 @@ def install(args: InstallPackageArgs):
 
     dest = Path(args.sysroot) / Path(args.prefix).relative_to("/")
 
-    scope = builder.TargetScope.use(args)
-    products = builder.build(scope, "all")
+    registry = model.Registry.use(args)
+    toInstall = registry.lookup(args.component, model.Component)
+    if not toInstall:
+        raise RuntimeError(f"Component {args.component} not found")
+    target = model.Target.use(args)
+    scope = builder.TargetScope(registry, target)
+
+    products = []
+
+    for c in toInstall.resolved[target.id].required:
+        print(f"Building {c}...")
+        component = registry.lookup(c, model.Component)
+        assert component, f"Component {args.component} not found"
+        products += builder.build(scope, [component])
 
     print(f"Installing to {dest}...")
     print(f"sysroot: {args.sysroot}")
@@ -32,19 +45,19 @@ def install(args: InstallPackageArgs):
     shell.mkdir(str(dest / "bin"))
     shell.mkdir(str(dest / "share"))
     for product in products:
-        component = product.component
-        print(f"Installing {component.id}...")
-        if component.type == model.Kind.EXE:
-            name = component.id
+        toInstall = product.component
+        print(f"Installing {toInstall.id}...")
+        if toInstall.type == model.Kind.EXE:
+            name = toInstall.id
             name = name.removesuffix(".main").removesuffix(".cli")
             shell.cp(str(product.path), str(dest / "bin" / name))
 
-        ressources = builder.listRes(component)
+        ressources = builder.listRes(toInstall)
         if ressources:
-            shell.mkdir(str(dest / "share" / component.id))
-            for res in builder.listRes(component):
-                rel = Path(res).relative_to(component.subpath("res"))
-                resDest = dest / "share" / component.id / rel
+            shell.mkdir(str(dest / "share" / toInstall.id))
+            for res in builder.listRes(toInstall):
+                rel = Path(res).relative_to(toInstall.subpath("res"))
+                resDest = dest / "share" / toInstall.id / rel
                 resDest.parent.mkdir(parents=True, exist_ok=True)
                 shell.cp(str(res), str(resDest))
 
